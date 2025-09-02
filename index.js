e506f5227520dc8243e36d730ce1a1c371da34ea
Veda361 <devranjeetq@gmail.com>
2025-09-02 20:50:24 +0530

    first commit

--- index.js
+++ index.js
@@ -1,66 +1,78 @@
-const fs = require('fs');
+const { readFileSync } = require('fs');
+const express = require('express');
 
-const http = require('http')
+const index = readFileSync('index.html', 'utf-8');
+const data = JSON.parse(readFileSync('dummy.json', 'utf-8'));
+const products = data.products;
 
-const index = fs.readFileSync('index.html', 'utf-8');
-const jsonData = JSON.parse(fs.readFileSync('dummy.json', 'utf-8'));
-const products = jsonData.products;
-
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.method)
-
-  // Handle /product/:id route  
-  if (req.url.startsWith('/product')) {
-    const id = req.url.split('/')[2];
-    //req.url is the URL path from the HTTP request (e.g., /product/3).
-
-    // .split('/') splits the string into an array using / as the separator.
-    
-    // [2] gets the third element of the array (arrays are zero-indexed).
+const server = express();
+const morgan = require('morgan');
 
 
-    // Example:
 
-    // If req.url is /product/3:
+//custom middleware
+server.use(express.json())
+// server.use(express.urlencoded())
 
-    // req.url.split('/') gives ['', 'product', '3']
-    // [2] gets '3'
-    // So:
-    // This line extracts the product ID from a URL like /product/3.
-    const prd = products.find(p => p.id == id); // match both string or number
-    console.log("Requested URL:", req.url);
-    console.log("Extracted ID:", id);
-    console.log("Matched Product:", prd);
+server.use(express.static('public'))
 
-    res.setHeader('Content-Type', 'text/html');
-    if (prd) {
-      let modifiedIndex = index
-        .replace('**HeadPhones**', prd.title)
-        .replace('**price**', prd.price)
-      res.end(modifiedIndex);
+server.use(morgan('default'))
+
+
+//middleware
+// server.use((req, res, next) => {
+//     console.log(
+//         req.get('User-Agent'),
+//         req.method,
+//         req.pipe,
+//         req.hostname,
+//         new Date())
+//     next()
+// })
+
+const auth = (req, res, next) => {
+    console.log()
+    if (req.body.password == '123') {
+        next()
     } else {
-      res.writeHead(404);
-      res.end('Product not found');
+        res.sendStatus(401)
     }
-    return; // Prevents further response
-  }
 
-  switch (req.url) {
-    case '/':
-      res.setHeader('content-Type', 'text/html')
-      res.end(index)
-      break
-    case '/api':
-      res.setHeader('Content-Type', 'application/json')
-      res.end(JSON.stringify(jsonData))
-      break
-    default:
-      res.writeHead(404)
-      res.end()
-  }
+}
 
-  console.log('server started')
+
+//API END-POINT 
+
+server.get('/', auth, (req, res) => {
+    res.json({ type: 'GET' })
+})
+
+server.post('/', auth, (req, res) => {
+    res.json({ type: 'POST' })
+})
+
+server.put('/', auth, (req, res) => {
+    res.json({ type: 'PUT' })
+})
+
+server.delete('/', auth, (req, res) => {
+    res.json({ type: 'DELETE' })
+})
+
+server.patch('/', auth, (req, res) => {
+    res.json({ type: 'PATCH' })
+})
+
+
+server.get('/', auth, (req, res) => {  //this is how an any api shoul be made through
+    // res.status(201).send('<h1>hello</h1>')
+    // res.sendStatus(404)
+    // res.send('<h1>hello<./h1>')
+    //  res.sendFile(path.join('/Users/Dev%20Sahu/Desktop/Node_app/index.html'));
+    // res.json(products)
 
 })
 
-server.listen(8080);
+server.listen(8080, () => {
+    console.log('server started')
+});
